90255f7719f19270f59d7cf85ef405c6b25f6989
pndaly <pndaly@arizona.edu>
2021-02-09 10:10:59 -0700

    Added airmass

--- cron/sassy_cron.sh
+++ cron/sassy_cron.sh
@@ -333,6 +333,7 @@ _add_classifier_and_plot () {
     write_yellow "DryRun> python3 ${SASSY_SRC}/utils/sassy_cron_alerce.py"
     write_yellow "DryRun> python3 ${SASSY_SRC}/utils/sassy_cron_mollweide.py --png=sassy_cron_mollweide.png --verbose"
     write_yellow "DryRun> python3 ${SASSY_SRC}/utils/sassy_cron_finders.py --folder=${SASSY_SRC}/static/img --verbose"
+    write_yellow "DryRun> conda deactivate"
     write_yellow "DryRun> python3 ${SASSY_SRC}/utils/sassy_cron_airmass.py --folder=${SASSY_SRC}/static/img --verbose"
     write_yellow "DryRun> chown www-data:www-data ${SASSY_SRC}/static/img/ZTF2*.png >> /dev/null 2>&1"
     write_yellow "DryRun> chown www-data:www-data ${SASSY_SRC}/static/img/ZTF2*.jpg >> /dev/null 2>&1"
@@ -347,6 +348,7 @@ _add_classifier_and_plot () {
     python3 ${SASSY_SRC}/utils/sassy_cron_alerce.py
     python3 ${SASSY_SRC}/utils/sassy_cron_mollweide.py --png=sassy_cron_mollweide.png --verbose
     python3 ${SASSY_SRC}/utils/sassy_cron_finders.py --folder=${SASSY_SRC}/static/img --verbose
+    conda deactivate
     python3 ${SASSY_SRC}/utils/sassy_cron_airmass.py --folder=${SASSY_SRC}/static/img --verbose
     chown www-data:www-data ${SASSY_SRC}/static/img/ZTF2*.png >> /dev/null 2>&1
     chown www-data:www-data ${SASSY_SRC}/static/img/ZTF2*.jpg >> /dev/null 2>&1
